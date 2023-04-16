7046855969ec1a83637fd59c25141dab4e49ac99
ex3cute22 <58621880+ex3cute22@users.noreply.github.com>
2023-04-16 12:25:32 +0700

    Initial Commit

---
+++ TDD3_BarleyBreak/TDD3_BarleyBreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDD3_BarleyBreakApp.swift
+//  TDD3_BarleyBreak
+//
+//  Created by Илья Викторов on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TDD3_BarleyBreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
